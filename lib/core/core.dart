d5d5552b912a85041e047b4b7742ebcc154a7db3
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-01 02:49:27 +0300

    feat: expose Firebase foundation services

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,3 +1,6 @@
 export 'app/app_config.dart';
 export 'security/security_policy.dart';
 export 'storage/app_storage.dart';
+export 'firebase/firebase_service.dart';
+export 'firebase/firebase_options.dart';
+export 'services/auth_service.dart';
