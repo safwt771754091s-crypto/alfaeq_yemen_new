08639ab2cad6b49c95c3278180d6c1cc60405514
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-01 02:49:23 +0300

    feat: add Firebase initialization service

---
+++ lib/core/firebase/firebase_service.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+class FirebaseService {
+  const FirebaseService._();
+
+  static Future<void> initialize() async {
+    if (!DefaultFirebaseOptions.isConfigured) {
+      return;
+    }
+
+    await Firebase.initializeApp();
+  }
+}
