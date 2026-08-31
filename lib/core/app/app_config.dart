844ba2772bcf89ab6c10ab9310f31d9e68229cb3
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-01 02:46:58 +0300

    feat: add core application configuration

---
+++ lib/core/app/app_config.dart
@@ -0,0 +1,9 @@
+class AppConfig {
+  const AppConfig._();
+
+  static const appName = 'الفائق يمن';
+  static const appTagline = 'الفائق.. يوصل لبي ما يوصل';
+  static const defaultLocale = 'ar';
+  static const defaultCurrency = 'YER';
+  static const supportedLocales = ['ar', 'en'];
+}
