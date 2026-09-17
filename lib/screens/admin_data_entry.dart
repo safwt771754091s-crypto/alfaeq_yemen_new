f5c16c740a9c47d6bcc37337e7f5e8416e1b2b55
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-17 03:44:43 +0300

    fix: remove unused flutter_map import from admin data entry

--- lib/screens/admin_data_entry.dart
+++ lib/screens/admin_data_entry.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 import '../core/app_sections.dart';
