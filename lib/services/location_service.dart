341ead65121bd142c6b26ee92667fce1abb16c11
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-17 03:42:47 +0300

    Add distance calculation for store-to-customer delivery radius

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -25,4 +27,24 @@ class LocationService {
 
   static GeoPoint toGeoPoint(Position position) =>
       GeoPoint(position.latitude, position.longitude);
+
+  /// Straight-line distance between two coordinates, in kilometres.
+  static double distanceKm({
+    required double latitude1,
+    required double longitude1,
+    required double latitude2,
+    required double longitude2,
+  }) {
+    const earthRadiusKm = 6371.0088;
+    final dLat = _degToRad(latitude2 - latitude1);
+    final dLon = _degToRad(longitude2 - longitude1);
+    final lat1 = _degToRad(latitude1);
+    final lat2 = _degToRad(latitude2);
+    final a = math.pow(math.sin(dLat / 2), 2) +
+        math.cos(lat1) * math.cos(lat2) * math.pow(math.sin(dLon / 2), 2);
+    final c = 2 * math.atan2(math.sqrt(a.toDouble()), math.sqrt(1 - a.toDouble()));
+    return earthRadiusKm * c;
+  }
+
+  static double _degToRad(double value) => value * math.pi / 180;
 }
