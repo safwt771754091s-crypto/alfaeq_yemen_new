882286a5a6e89ed26599007456065ea42d835962
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-17 03:52:06 +0300

    feat: start and stop user presence with app lifecycle

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'admin_dashboard.dart';
@@ -14,12 +17,45 @@ class AuthGate extends StatefulWidget {
   State<AuthGate> createState() => _AuthGateState();
 }
 
-class _AuthGateState extends State<AuthGate> {
+class _AuthGateState extends State<AuthGate> with WidgetsBindingObserver {
   final AuthService _auth = AuthService();
+  StreamSubscription<User?>? _authSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _authSubscription = _auth.authStateChanges.listen((user) {
+      if (user != null) {
+        _auth.startPresence();
+      } else {
+        _auth.stopPresence();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _authSubscription?.cancel();
+    _auth.stopPresence();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final user = _auth.auth.currentUser;
+    if (user == null) return;
+    if (state == AppLifecycleState.resumed) {
+      _auth.startPresence();
+    } else if (state == AppLifecycleState.paused || state == AppLifecycleState.detached) {
+      _auth.stopPresence();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
+    return StreamBuilder<User?>(
       stream: _auth.authStateChanges,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -35,7 +71,6 @@ class _AuthGateState extends State<AuthGate> {
             }
 
             final role = roleSnapshot.data ?? 'customer';
-
             switch (role) {
               case 'owner':
               case 'admin':
