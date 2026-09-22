13308ba1c9b7957326e56698b2ed4f6c915e7dfc
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-22 04:56:51 +0300

    fix: correct latlong import for tracking map

--- lib/screens/live_tracking_map_page.dart
+++ lib/screens/live_tracking_map_page.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 import '../services/supabase_service.dart';
 
 class LiveTrackingMapPage extends StatelessWidget {
