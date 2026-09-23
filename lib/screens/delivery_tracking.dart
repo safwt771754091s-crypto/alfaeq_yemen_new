c38f4793386747cbb2b2c171abbda300f4e42ee1
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-23 18:13:07 +0300

    refactor: migrate user session to Supabase Auth

--- lib/screens/delivery_tracking.dart
+++ lib/screens/delivery_tracking.dart
@@ -1,6 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
 import '../services/firestore_service.dart';
 import '../services/supabase_service.dart';
 import 'live_tracking_map_page.dart';
@@ -67,7 +66,7 @@ class DeliveryTracking extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentUid = FirebaseAuth.instance.currentUser?.uid;
+    final currentUid = const AuthService().currentAuthUser?.uid;
     return Directionality(textDirection: TextDirection.rtl, child: Scaffold(
       appBar: AppBar(title: const Text('تتبع الطلب', style: TextStyle(fontWeight: FontWeight.w900))),
       body: currentUid == null
