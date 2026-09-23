68004198d778e2e810e2d68332a808fde15e8fb4
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-23 18:12:52 +0300

    refactor: migrate user session to Supabase Auth

--- lib/screens/my_orders_page.dart
+++ lib/screens/my_orders_page.dart
@@ -1,6 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
 import '../services/firestore_service.dart';
 import '../services/supabase_service.dart';
 import 'delivery_tracking.dart';
@@ -10,7 +9,7 @@ class MyOrdersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
+    final user = const AuthService().currentUser;
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
