4bdf466028a42830431d682778d2c7db1bb640f2
safwt <226787606+safwt771754091s-crypto@users.noreply.github.com>
2026-09-26 01:05:29 +0300

    remove direct Firebase auth dependency

--- lib/ai/ai_tools.dart
+++ lib/ai/ai_tools.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_ai/firebase_ai.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../services/supabase_service.dart';
 import '../services/firestore_service.dart';
 
